c860f6e67871c109785b374a5d6bf1541a5da995
Maxim Turusov <turusovmaxim@gmail.com>
2021-06-24 23:26:59 +0300

    using constraintLayout for itemView

--- app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/controllers/CrimeFragment.kt
@@ -36,7 +36,7 @@ class CrimeFragment: Fragment() {
 
         titleField = view?.findViewById(R.id.crime_title) as EditText
         dateButton = view?.findViewById(R.id.crime_date) as Button
-        solvedCheckBox = view?.findViewById(R.id.crime_solved) as CheckBox
+        solvedCheckBox = view?.findViewById(R.id.crime_solved_img) as CheckBox
 
         return inflater.inflate(R.layout.fragment_crime, container, false)
     }
